ba718f53d8818c38d55b050f85c574ffbcfba510
matia@LAPTOP-U50VL48J <matiasalvealc@gmail.com>
2024-06-11 19:03:01 -0400

    agregar dia 2

---
+++ M6_Desarollo_de_aplicaciones_JEE_con_SPRING_framework/1.Gestor_de_proyectos/dia2/HolaMundoSpringBoot/src/test/java/cl/ejemplosb/demo/HolaMundoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ejemplosb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
