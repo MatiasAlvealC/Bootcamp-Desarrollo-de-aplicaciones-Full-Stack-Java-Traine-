193512c7f3b879bbdc60ea352723881ac3cf07ab
matia@LAPTOP-U50VL48J <matiasalvealc@gmail.com>
2024-06-12 22:04:03 -0400

    cambios dia2

--- M6_Desarollo_de_aplicaciones_JEE_con_SPRING_framework/1.Gestor_de_proyectos/dia2/HolaMundoSpringBoot/src/main/java/cl/ejemplosb/demo/Home.java
+++ M6_Desarollo_de_aplicaciones_JEE_con_SPRING_framework/1.Gestor_de_proyectos/dia2/HolaMundoSpringBoot/src/main/java/cl/ejemplosb/demo/Home.java
@@ -1,18 +1,25 @@
 package cl.ejemplosb.demo;
 
-import org.springframework.stereotype.Controller;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
+	
+	private static final Logger logger = LogManager.getLogger(Home.class);
+
 	@GetMapping("/saludar")
 	public String hola() {
+		logger.info("Acceso a la URL /saludar");
 		return "Hola Mundo";
 	}
 	
 	@GetMapping("/adios")
 	public String adios() {
+		logger.info("Acceso a la URL /adios");
 		return "Adios Mundo";
 	}
 }
